e1b0495968f9fea45f85e58839c247c79b376c7a
kevindoubleu <kevin.wibamanto@gmail.com>
2023-06-30 12:44:04 +0700

    feat: add dir_bf attack functionality

--- vulnseek/attacks/dir_bf.py
+++ vulnseek/attacks/dir_bf.py
@@ -1,12 +1,13 @@
 from attacks.interface import AttackInterface
 from utils.prompt import *
+import requests
 
 class DirectoryBruteForce(AttackInterface):
     def title(self):
         return "Directory Brute Force"
     
     def prompt_for_params(self):
-        self.url = prompt_param_required("URL")
+        self.url = prompt_param_required("URL with scheme (e.g. http://127.0.0.1:8080)")
         self.path_to_wordlist = prompt_param_with_default_value_and_name(
             "path to the wordlist",
             "resources/dir_bf/small.txt",
@@ -17,9 +18,21 @@ class DirectoryBruteForce(AttackInterface):
         wordlist = open(self.path_to_wordlist, "r")
         print("reading from wordlist file", self.path_to_wordlist)
 
-        for line in wordlist:
-            print("trying", self.url + "/" + line.rstrip("\n"))
+        found_count = 0
+        found_endpoints = []
+        for endpoint in wordlist:
+            endpoint = endpoint.rstrip("\n")
+            full_url = self.url + "/" + endpoint
+            print(f"trying {full_url}")
 
+            r = requests.get(full_url)
+            if r.status_code == 200:
+                print(f"found endpoint {endpoint}")
+                found_count += 1
+                found_endpoints.append("/"+endpoint)
+
+        print(f"found {found_count} endpoints")
+        print(found_endpoints)
         input()
         wordlist.close()
         return
