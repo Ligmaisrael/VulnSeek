0788a00a7b77358281e65dc00708cc2730ac502d
kevindoubleu <kevin.wibamanto@gmail.com>
2023-07-23 13:54:12 +0700

    feat: add view scan detail from view history

--- vulnseek/utils/scan_detail.py
+++ vulnseek/utils/scan_detail.py
@@ -1,11 +1,8 @@
-import os
-
-from model.history import HistoryStore
 from model.loot import LootStore
 from model.structure.history import HistoryStructure
-from model.structure.loot import LootStructure
 from prettytable import PrettyTable
 from utils.export import export_to_md_file
+from utils.print import clear_screen_with_message
 from utils.prompt import validate_number_selection
 
 loot_id = "Loot id"
@@ -34,24 +31,16 @@ class ScanDetail:
 
     def view(self):
         loots = self.loot_store.get_by_scan_id(self.scan_id)
-        loots = shorten_resp_body(loots)
-        loots = add_newline_after_each_row(loots)
+        tbl = view_as_table(loots)
 
-        tbl = PrettyTable(ScanDetail.headers)
-        tbl.add_rows(loots)
-        tbl.del_column(loot_id)
-        tbl.del_column(scan_id)
-        tbl._max_width = {resp_headers: 50, resp_body: 50}
-        tbl.align[resp_headers] = "l"
-        tbl.align[resp_body] = "l"
-
-        self.scan_detail_menu(tbl.get_string())
+        if len(tbl) > 0:
+            self.scan_detail_menu(tbl)
 
     def scan_detail_menu(self, detail_view: str):
         print(detail_view)
         while True:
             print("1. Save full scan result to file")
-            print("0. Back to main menu")
+            print("0. Back")
             choice = validate_number_selection(max=1)
 
             if choice == 0:
@@ -60,6 +49,25 @@ class ScanDetail:
                 success = export_to_md_file(self.scan_id)
                 if success:
                     break
+
+
+def view_as_table(loots: list) -> str:
+    if len(loots) == 0:
+        clear_screen_with_message("No payloads were successful during that scan")
+        return ""
+
+    loots = shorten_resp_body(loots)
+    loots = add_newline_after_each_row(loots)
+
+    tbl = PrettyTable(ScanDetail.headers)
+    tbl.add_rows(loots)
+    tbl.del_column(loot_id)
+    tbl.del_column(scan_id)
+    tbl._max_width = {resp_headers: 50, resp_body: 50}
+    tbl.align[resp_headers] = "l"
+    tbl.align[resp_body] = "l"
+
+    return tbl.get_string()
 
 
 def shorten_resp_body(loots: list) -> list:
--- vulnseek/utils/scan_history.py
+++ vulnseek/utils/scan_history.py
@@ -1,6 +1,8 @@
 from model.history import HistoryStore
 from model.structure.history import HistoryStructure
 from prettytable import PrettyTable
+from utils.prompt import validate_number_selection
+from utils.scan_detail import ScanDetail
 
 
 class ScanHistory:
@@ -8,6 +10,7 @@ class ScanHistory:
 
     def __init__(self):
         self.history = HistoryStore()
+        self.db_rows = None
 
     def get_last(self) -> HistoryStructure:
         db_row = self.history.view_latest_one()
@@ -15,10 +18,34 @@ class ScanHistory:
         return history_row
 
     def view_history(self):
-        db_rows = self.history.view_latest_n(50, 0)
+        self.db_rows = self.history.view_latest_n(50, 0)
         tbl = PrettyTable(ScanHistory.headers)
-        tbl.add_rows(db_rows)
+        tbl.add_rows(self.db_rows)
         tbl.del_column(ScanHistory.headers[0])
+        add_numbers_col(tbl)
 
-        print(tbl)
-        input()
+        self.scan_history_menu(tbl.get_string())
+
+    def scan_history_menu(self, history_view: str):
+        row_count = len(self.db_rows)
+        while True:
+            print(history_view)
+            print(f"1-{row_count}. View scan detail")
+            print("0. Back")
+            choice = validate_number_selection(max=row_count)
+
+            if choice == 0:
+                break
+            else:
+                scan = HistoryStructure.from_row(self.db_rows[choice - 1])
+                scan_detail = ScanDetail(scan)
+                scan_detail.view()
+
+
+def add_numbers_col(tbl: PrettyTable) -> PrettyTable:
+    fieldname = "No"
+    tbl._field_names.insert(0, fieldname)
+    tbl._align[fieldname] = "c"
+    tbl._valign[fieldname] = "t"
+    for i, _ in enumerate(tbl._rows):
+        tbl._rows[i].insert(0, i + 1)
